8b233dbba15229df1b2db600c8112796616160e2
Cole Beckwith <CBeckwith411@gmail.com>
2016-07-09 21:14:59 -0500

    get ingredients function expanded

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -22,6 +22,7 @@
     vm.getIngredients = getIngredients;
     vm.cart = [];
     vm.ingredients = [];
+    vm.shoppingList = [];
 
     activate();
 
@@ -33,12 +34,20 @@
     }
 
     function getIngredients() {
+      vm.ingredients = [];
       angular.forEach(vm.meals, function(meal) {
         if (meal.added === true) {
-          vm.ingredients += meal.description;
+          vm.ingredients.push(meal.description.split(', '));
         }
       });
-      console.log(vm.ingredients);
+      angular.forEach(vm.ingredients, function(recipe) {
+        vm.shoppingList += recipe.join(", ") + ", "
+      });
+      //This doesn't feel like the best way to do it, but I'm struggling to come up with a more concise approach.
+      // A way to avoid the whole thing might be to add the ingredients upon adding the menu item, but then you have to
+      // remove the ingredients if the item is removed which seems to be the more complex route.
+      // Let me know what you might do.
+      console.log(vm.shoppingList);
     }
 
     function showToastr() {
